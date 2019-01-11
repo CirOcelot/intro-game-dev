3c7f4e6220354c6fd8aaccda25aa32a4b7ac67a6
Jack Jain <cirocelot@gmail.com>
2019-01-11 08:44:09 -0700

    script was in head, added it to bottom of body tag in order for it to work

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ let canvas = document.getElementById("gameScreen");
 
 let ctx = canvas.getContext("2d");
 
+ctx.clearRect(0, 0, 800, 600);
+
 ctx.fillStyle = '#f00';
-ctx.fillRect(20,20,100,100);
+ctx.fillRect(20, 20, 50, 50);
 
