c415c8c2d1f82c360734c67fae16a04d5ee8ef61
Jack Jain <cirocelot@gmail.com>
2019-01-11 09:49:32 -0700

    added paddle class

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ let canvas = document.getElementById("gameScreen");
 
 let ctx = canvas.getContext("2d");
 
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
 ctx.clearRect(0, 0, 800, 600);
 
-ctx.fillStyle = '#f00';
-ctx.fillRect(20, 20, 50, 50);
 
---
+++ src/paddle.js
@@ -0,0 +1,11 @@
+class Paddle {
+	constructor(gameWidth, gameHeight){
+this.width = 150;
+this.height = 30;
+	}
+
+draw(ctx) {
+
+}
+
+}
